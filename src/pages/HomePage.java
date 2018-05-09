46147d33c39d6c6479671b3cd9193278ad721367
Selenium <Selenium@172.31.98.140>
2018-05-09 15:34:54 -0400

    home page

--- src/pages/HomePage.java
+++ src/pages/HomePage.java
@@ -1,5 +1,33 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class HomePage {
+	public WebDriver driver;
+	
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+	@FindBy(linkText="Sing in")
+	public WebElement signInLink;
+	
+	@FindBy(xpath="//img[@class='logo img-responsive']")
+	public WebElement logo;
+	
+	public boolean isAt() {
+		return driver.getTitle().equals("My Store");
+		
+	}
+	
+	public void gotoLoginPage() {
+		signInLink.click();
+	}
+	
 
 }
